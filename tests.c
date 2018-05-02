4c36a523b0acc3459a2cf095e8166ce29226f42b
Yannik Enss <account-gitlab@yannikenss.de>
2018-05-02 13:26:20 +0000

    fixed unit tests

--- tests.c
+++ tests.c
@@ -9,3 +9,32 @@ void add_test(void) {
     CU_ASSERT(add(1,2) == 3);
     CU_ASSERT(add(314,232) == 546);
 }
+
+int main() {
+    CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", NULL, NULL);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if (NULL == CU_add_test(pSuite, "test of add()", add_test))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
+}
